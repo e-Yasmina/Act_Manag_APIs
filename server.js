1643ed23addd24b7ad9f63077afa20da72dc662c
e-Yasmina <yasminaelbernoussi@gmail.com>
2025-02-01 13:29:36 +0100

    adding cross

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const cors = require("cors");
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
@@ -7,7 +7,7 @@ const PORT = 3000;
 //app.use(express.json());
 
 // Enable CORS for all routes
-//app.use(cors());
+app.use(cors());
 
 // In-memory storage for first name and last name
 let users = [];
